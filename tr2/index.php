81285e1df3cdcf2f1255276b483fd0fc477f8b50
note <extended88@gmail.com>
2022-12-20 17:56:13 +0900

    create php files

---
+++ tr2/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$hello = "hello world";
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git training3</title>
+</head>
+<body>
+    <h1>Git training3</h1>
+    <p><?php echo $hello; ?></p>
+</body>
+</html>
